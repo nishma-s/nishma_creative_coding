89e4bab6becd90c3f9a90e561687766528e03683
Glitch (code-portfolio-template) <none>
2021-04-08 01:38:28 +0000

    🍖🛀 Checkpoint ./p2.html:25153541/5709 ./ml.js:25153541/3683 ./index.html:25153541/164

--- ml.js
+++ ml.js
@@ -12,7 +12,7 @@ This example uses p5 preload function to create the classifier
 // Classifier Variable
 let classifier;
 // Model URL
-let imageModelURL = 'https://teachablemachine.withgoogle.com/models/96tukx8xE/';
+let imageModelURL = "https://teachablemachine.withgoogle.com/models/96tukx8xE/";
 
 // Video
 let video;
@@ -24,22 +24,22 @@ let y;
 
 // Load the model first
 function preload() {
-  classifier = ml5.imageClassifier(imageModelURL + 'model.json');
+  classifier = ml5.imageClassifier(imageModelURL + "model.json");
 }
 
-
 function setup() {
-  createCanvas(320, 260);
+  let c = createCanvas(320, 260);
+  c.parent("canvas-holder");
   // Create the video
   video = createCapture(VIDEO);
   video.size(320, 240);
   video.hide();
 
-  flippedVideo = ml5.flipImage(video)
+  flippedVideo = ml5.flipImage(video);
   // Start classifying
   classifyVideo();
-  x = width/2;
-  y = height/2;
+  x = width / 2;
+  y = height / 2;
 }
 
 function draw() {
@@ -52,28 +52,28 @@ function draw() {
   textSize(16);
   textAlign(CENTER);
   text(label, width / 2, height - 4);
- 
-  if(label=="Class 1"){
+
+  if (label == "Class 1") {
     y--;
   }
-  if(label=="Class 2"){
+  if (label == "Class 2") {
     y++;
   }
-  if(label=="Class 3"){
+  if (label == "Class 3") {
     x--;
   }
-  if(label=="Class 4"){
+  if (label == "Class 4") {
     x++;
   }
 
-   ellipse(x, y, 20, 20);
+  ellipse(x, y, 20, 20);
   // print("x:"+ x);
   //  print("y:"+ y);
 }
 
 // Get a prediction for the current video frame
 function classifyVideo() {
-  flippedVideo = ml5.flipImage(video)
+  flippedVideo = ml5.flipImage(video);
   classifier.classify(flippedVideo, gotResult);
 }
 
